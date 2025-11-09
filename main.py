9a56c55a7c20ab81adecc758b48b987e9647b215
TabitoSaito <domi2601.hoelzl@gmail.com>
2025-11-09 19:00:57 +0100

    Add .env.example and .gitignore. Reformat code

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap5
 import numpy as np
 from PIL import Image
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 FILETYPES = [".png", ".jpg"]
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 Bootstrap5(app)
 
 
@@ -19,10 +23,10 @@ def home():
             top_n_c = process_img(f.filename)
             return render_template("index.html", name=f.filename, colors=top_n_c)
         elif f.filename == "standard.jpg":
-            flash('Filename clashes with preassigned File. Please rename your file.')
+            flash("Filename clashes with preassigned File. Please rename your file.")
             return redirect(url_for("home"))
         else:
-            flash('Wrong filetype!')
+            flash("Wrong filetype!")
             return redirect(url_for("home"))
     if request.method == "GET":
         top_n_c = process_img("standard.jpg")
@@ -42,8 +46,12 @@ def process_img(file: str) -> dict:
             size_test = False
         except ValueError:
             n -= 1
-    top_n_colors_tuples = [tuple(color) for color in unique_colors[top_n_colors_pos].tolist()]
-    top_n_colors_dict = {k: v for (k, v) in zip(top_n_colors_tuples, counts[top_n_colors_pos].tolist())}
+    top_n_colors_tuples = [
+        tuple(color) for color in unique_colors[top_n_colors_pos].tolist()
+    ]
+    top_n_colors_dict = {
+        k: v for (k, v) in zip(top_n_colors_tuples, counts[top_n_colors_pos].tolist())
+    }
 
     return top_n_colors_dict
 
